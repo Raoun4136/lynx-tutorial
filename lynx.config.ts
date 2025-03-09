8745ec6ea698b46ec9948ae63c02e287a47dc66d
SeongOh Park <qkrtjddh1212@naver.com>
2025-03-09 21:55:31 +0900

    🚧 Create Gallery Tutorial 1

--- lynx.config.ts
+++ lynx.config.ts
@@ -1,16 +1,29 @@
-import { defineConfig } from '@lynx-js/rspeedy'
+import { defineConfig } from '@lynx-js/rspeedy';
 
-import { pluginQRCode } from '@lynx-js/qrcode-rsbuild-plugin'
-import { pluginReactLynx } from '@lynx-js/react-rsbuild-plugin'
+import { pluginQRCode } from '@lynx-js/qrcode-rsbuild-plugin';
+import { pluginReactLynx } from '@lynx-js/react-rsbuild-plugin';
+import { pluginSass } from '@rsbuild/plugin-sass';
 
 export default defineConfig({
+  source: {
+    entry: {
+      FirstImageCard: './src/FirstImageCard/index.tsx',
+      // LikeImageCard: './src/LikeImageCard/index.tsx',
+      CreateGallery: './src/CreateGallery/index.tsx',
+      AddAutoScroll: './src/AddAutoScroll/index.tsx',
+      AddNiceScrollbar: './src/AddNiceScrollbar/index.tsx',
+      // ScrollbarCompare: './src/ScrollbarCompare/index.tsx',
+      // GalleryComplete: './src/GalleryComplete/index.tsx',
+    },
+  },
   plugins: [
     pluginQRCode({
       schema(url) {
         // We use `?fullscreen=true` to open the page in LynxExplorer in full screen mode
-        return `${url}?fullscreen=true`
+        return `${url}?fullscreen=true`;
       },
     }),
     pluginReactLynx(),
+    pluginSass(),
   ],
-})
+});
